1048faddab1df4ba56509a86fca4cc88277b994d
Declan Malone <declanamalone@gmail.com>
2019-03-01 21:18:55 +0000

    started on JobController

--- recruitment_app/back_end/src/main/java/com/example/codeclan/recruitment/controllers/JobController.java
+++ recruitment_app/back_end/src/main/java/com/example/codeclan/recruitment/controllers/JobController.java
@@ -1,4 +1,13 @@
 package com.example.codeclan.recruitment.controllers;
 
+
+import com.example.codeclan.recruitment.repositories.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/jobs")
 public class JobController {
 }
