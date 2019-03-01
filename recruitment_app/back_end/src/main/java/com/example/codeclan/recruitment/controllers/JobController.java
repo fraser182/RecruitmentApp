7f995dae4cf2b512e13a46bd4d740704d1debe8a
Declan Malone <declanamalone@gmail.com>
2019-03-01 22:40:08 +0000

    added breif.md

--- recruitment_app/back_end/src/main/java/com/example/codeclan/recruitment/controllers/JobController.java
+++ recruitment_app/back_end/src/main/java/com/example/codeclan/recruitment/controllers/JobController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
-@RequestMapping(value = "/jobs")
+@RequestMapping("/jobs")
 public class JobController {
+
+    @Autowired
+    JobRepository jobRepository;
+
 }
--- recruitment_app/back_end/src/main/java/com/example/codeclan/recruitment/repositories/JobRepository.java
+++ recruitment_app/back_end/src/main/java/com/example/codeclan/recruitment/repositories/JobRepository.java
@@ -2,8 +2,9 @@ package com.example.codeclan.recruitment.repositories;
 
 import com.example.codeclan.recruitment.models.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface JobRepository extends JpaRepository<Job, Long> {
+@RepositoryRestResource
+public interface JobRepository extends JpaRepository<Job, Long>, JobRepositoryCustom{
 }
