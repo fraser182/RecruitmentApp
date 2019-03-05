d04908fa5f028818c50ad4eaea4eb7f3c926d7e4
Fraser Ross <fraser182@icloud.com>
2019-03-05 23:19:26 +0000

    started positioning jobs container

--- recruitment_app/front_end/recruitment_viewer/src/components/jobs/Job.js
+++ recruitment_app/front_end/recruitment_viewer/src/components/jobs/Job.js
@@ -6,31 +6,52 @@ const Job = (props) => {
   if (!props.job){
     return null
   }
-const expandDivId = `L` + props.job.dateCreated;
-const hashExpandedDivId = `#` + expandDivId;
+  const expandDivId = `L` + props.job.dateCreated;
+  const hashExpandedDivId = `#` + expandDivId;
 
   return (
 
     <div className="job" id={expandDivId}>
 
-    <p> {props.job.title}</p>
-    <p> {props.job.jobType}</p>
-    <p>{props.job.salary}</p>
+      <div className="job-upper">
+        <div className="job-upper-left">
+          <h5>{props.job.title}</h5>
+          <p> {props.job.jobType}</p>
+        </div>
+
+        <div className="job-upper-middle">
+          <h5> {props.job.companyName} </h5>
+          <p> {props.job.location}</p>
+        </div>
+
+        <div className="job-upper-right">
+          <h5> {props.job.skillLevel}</h5>
+          <p> £ {props.job.salary}</p>
+        </div>
+
+    </div>
+
     <UncontrolledCollapse toggler={hashExpandedDivId}>
 
+    <div className="job-lower">
+      <div className="job-lower-left">
+        <p>{props.job.techStack}</p>
+        <p>{props.job.applicationUrl}</p>
 
-    <p>{props.job.skillLevel}</p>
-    <p>{props.job.techStack}</p>
-    <p> {props.job.companyName} </p>
-    <p> {props.job.location}</p>
-    <p>{props.job.applicationUrl}</p>
-    <p>{props.job.yourTeam}</p>
-    <p>{props.job.responsibilities}</p>
-    <p>{props.job.companyCulture}</p>
-    <p> {props.job.yourTeam} </p>
+        </div>
+
+        <div className="job-lower-right">
+        <p>{props.job.yourTeam}</p>
+        <p>{props.job.responsibilities}</p>
+        <p>{props.job.companyCulture}</p>
+        <p> {props.job.yourTeam} </p>
+        </div>
+
+      </div>
 
     </UncontrolledCollapse>
-      </div>
+
+  </div>
   )
 }
 
