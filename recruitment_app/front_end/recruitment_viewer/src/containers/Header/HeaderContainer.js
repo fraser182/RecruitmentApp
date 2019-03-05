1d8c9d6b95232b268446983f8548bdb47d0ebb03
Declan Malone <declanamalone@gmail.com>
2019-03-05 19:09:31 +0000

    added in functioning filter for role, skill, and type and navbar

--- recruitment_app/front_end/recruitment_viewer/src/containers/Header/HeaderContainer.js
+++ recruitment_app/front_end/recruitment_viewer/src/containers/Header/HeaderContainer.js
@@ -3,40 +3,64 @@ import '../../css/HeaderContainer.css';
 import { Container } from 'reactstrap';
 import { BrowserRouter as Switch, Route, Link } from 'react-router-dom';
 import About from '../../components/about.js'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
 
-const HeaderContainer = (props) => {
-  return (
-      <div className="header-container">
-        <div className="header-left-techjobs">
+  class HeaderContainer extends React.Component {
+
+    constructor(props) {
+      super(props);
+
+      this.toggle = this.toggle.bind(this);
+      this.state = {
+        isOpen: false
+      };
+    }
+
+    toggle() {
+      this.setState({
+        isOpen: !this.state.isOpen
+      });
+    }
+    render(){
+      return (
         <Container fluid>
-          <h1 className="display-3">Tech Jobs</h1>
-          <p className="lead">This is the new way to recruit tech.</p>
+        <h1 className="display-3">Tech Jobs</h1>
+        <p className="lead">This is the new way to recruit tech.</p>
+
+
+        <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Home</NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+        <Nav className="ml-auto" navbar>
+        <NavItem>
+        <NavLink href="/components/">Pricing</NavLink>
+        </NavItem>
+        <NavItem>
+        <NavLink href="/jobs/new">Post a Job</NavLink>
+        </NavItem>
+        <NavItem>
+        <NavLink href="/jobs/new">About</NavLink>
+        </NavItem>
+        </Nav>
+        </Collapse>
+        </Navbar>
         </Container>
-        </div>
-          <div className="header-right">
-            <div className="text-right">
 
-              <ul>
-                <li className="navLink">
-                  <Link to='/jobs/new'>Post A Job</Link>
-                </li>
-                <li className="navLink">
-                    <Link to="/pricing">Pricing</Link>
-                </li>
-                <li className="navLink">
-                  <Link to="#"><About />About</Link>
+      );
+    }
+  }
 
-                </li>
-                <li className="navLink">
-                  <Link to="#">Contact</Link>
-                </li>
-              </ul>
-
-            </div>
-          </div>
-        </div>
-  )
-}
-
-export default HeaderContainer;
+  export default HeaderContainer;
